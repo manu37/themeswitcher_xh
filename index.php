ac672ec1bb0e4df3a495a7a9effceb12619ea4ae
cmb69 <cmbecker69@gmx.de>
2014-05-20 11:20:33 +0000

    added basic plugin structure

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * main ;)
+ *
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Themeswitcher
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2014 Christoph M. Becker <http://3-magi.net>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Themeswitcher_XH
+ */
+
+/*
+ * Prevent direct access.
+ */
+if (!defined('CMSIMPLE_XH_VERSION')) {
+    header('HTTP/1.0 403 Forbidden');
+    exit;
+}
+
+?>
