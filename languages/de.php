81f467354308c6e036c5dc36fa51b4ab1dcf9523
Christoph M. Becker <cmbecker69@gmx.de>
2015-02-14 14:43:12 +0100

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,8 @@
+<?php
+
+$plugin_tx['themeswitcher']['label_theme']="Theme";
+$plugin_tx['themeswitcher']['label_activate']="Aktivieren";
+$plugin_tx['themeswitcher']['cf_prefer_page_theme']="Ob seitenspezifische Themes Vorrang vor der Benutzerauswahl haben.";
+$plugin_tx['themeswitcher']['cf_display_automatic']="Wann die Themeauswahl automatisch angezeigt werden soll: \"never\" (nie), \"frontend\" (nur im Frontend und im Ansichtsmodus), \"always\" (immer, außer in der Druckansicht).";
+
+?>
