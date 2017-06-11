a0d753364c94a1c2b199848cacd8ba820b4bc9b6
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-11 23:55:47 +0200

    Close #12: Modernize JS
    By restricting the progressive enhancements to contemporary browsers,
    we can nicely simplify the JavaScript code.

--- themeswitcher.js
+++ themeswitcher.js
@@ -17,59 +17,23 @@
  * along with Themeswitcher_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*jslint browser: true, maxlen: 80 */
-
 (function () {
     "use strict";
 
-    /**
-     * Calls a callback for each element.
-     *
-     * @param   {array-like} element
-     * @param   {Function}   callback
-     * @returns {undefined}
-     */
-    function forEach(objects, callback) {
-        var i, n;
-
-        for (i = 0, n = objects.length; i < n; i += 1) {
-            callback(objects[i]);
-        }
+    if (!("visibilityState" in document)) {
+        return;
     }
 
-    /**
-     * Registers an event listener.
-     *
-     * @param   {Object}    object
-     * @param   {string}    event
-     * @param   {Function}  listener
-     * @returns {undefined}
-     */
-    function on(object, event, listener) {
-        if (typeof object.addEventListener !== "undefined") {
-            object.addEventListener(event, listener, false);
-        } else if (typeof object.attachEvent !== "undefined") {
-            object.attachEvent("on" + event, listener);
-        }
-    }
+    var each = Array.prototype.forEach;
 
-    /**
-     * Initializes the themeswitcher select forms.
-     *
-     * @returns {undefined}
-     */
-    function initForms() {
-        forEach(document.forms, function (form) {
-            if (form.className === "themeswitcher_select_form") {
-                on(form.themeswitcher_select, "change", function (event) {
-                    var target = event.target || event.srcElement;
-
-                    target.form.submit();
-                });
-                form.getElementsByTagName("button")[0].style.display = "none";
-            }
+    document.addEventListener("DOMContentLoaded", function () {
+        each.call(document.getElementsByClassName("themeswitcher_select_form"), function (form) {
+            form.themeswitcher_select.addEventListener("change", function () {
+                this.form.submit();
+            });
+            each.call(form.getElementsByTagName("button"), function (button) {
+                button.style.display = "none";
+            });
         });
-    }
-
-    on(window, "load", initForms);
+    });
 }());
