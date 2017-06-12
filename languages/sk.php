7409226cd2ca08b9fadcdddd1606295a7ac8c43b
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-12 18:57:15 +0200

    Add Slovak translation by Tata

---
+++ languages/sk.php
@@ -0,0 +1,10 @@
+<?php
+
+$plugin_tx['themeswitcher']['label_theme']="Šablóna";
+$plugin_tx['themeswitcher']['label_activate']="Aktivovať";
+$plugin_tx['themeswitcher']['alt_logo']="Štylizovaná šablóna";
+$plugin_tx['themeswitcher']['cf_prefer_page_theme']="Majú mať šabl'pny stránky prednosť pred zvolenými?.";
+$plugin_tx['themeswitcher']['cf_display_automatic']="Má sa výber šablón zobrazovať: \"never\" (nikdy), \"frontend\" (iba hosťom a v náhľade), \"always\" (vždy okder tlače).";
+$plugin_tx['themeswitcher']['cf_allowed_themes']="Uveďte čiarkou oddelený zoznam voliteľných šablón. Názvy šablón môžu obsahovať aj masku * (hviedička) a ? (otáznik).";
+
+?>
